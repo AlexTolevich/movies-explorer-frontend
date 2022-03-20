8ee45a47cd8e74afe507e977c083d520f80be09e
AlexanderKuznetsov <alexanderanatolevic@yandex.ru>
2022-03-20 17:14:23 +0500

    feat: develop a component of the AboutProject

---
+++ src/components/AboutProject/AboutProject.js
@@ -0,0 +1,24 @@
+import './AboutProject.css';
+
+function AboutProject() {
+  return (
+    <div className="about-project">
+      <h2 className="about-project__header">О проекте</h2>
+      <div className="about-project__header-line"/>
+      <div className="about-project__table">
+        <h3 className="about-project__table-header">Дипломный проект включал 5 этапов</h3>
+        <h3 className="about-project__table-header">На выполнение диплома ушло 5 недель</h3>
+        <p className="about-project__table-text">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
+        <p className="about-project__table-text">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
+      </div>
+      <div className="about-project__periods">
+        <div className="about-project__periods-backend">1 неделя</div>
+        <div className="about-project__periods-frontend">4 недели</div>
+        <p className="about-project__periods-description">Back-end</p>
+        <p className="about-project__periods-description">Front-end</p>
+      </div>
+    </div>
+  )
+};
+
+export default AboutProject;
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,13 +1,15 @@
-import React  from 'react';
 import './Main.css';
-import Header from '../Header/Header.js';
-import Promo  from '../Promo/Promo.js';
+import React        from 'react';
+import Header       from '../Header/Header.js';
+import Promo        from '../Promo/Promo.js';
+import AboutProject from '../AboutProject/AboutProject.js';
 
 function Main({loggedIn}) {
   return (
     <>
       <Header loggedIn={loggedIn}/>
       <Promo/>
+      <AboutProject/>
     </>
   )
 }
