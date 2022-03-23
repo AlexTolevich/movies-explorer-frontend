2240c78f08066df707db4a955196f7c63fcf339a
AlexanderKuznetsov <alexanderanatolevic@yandex.ru>
2022-03-23 10:23:28 +0500

    style: opacity link

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -6,15 +6,15 @@ function Navigation({loggedIn}) {
     <nav className="navigation">
       {!loggedIn ?
         (<>
-          <Link to="#" className="header__link">Регистрация</Link>
-          <Link to="#" className="header__button">
+          <Link to="/signup" className="header__link">Регистрация</Link>
+          <Link to="/signin" className="header__button">
             <p className="header__button-text">Войти</p>
           </Link>
         </>) : (
           <>
-            <Link to="#" className="header__link header__link_is_logged-in header__link_is_active">Фильмы</Link>
-            <Link to="#" className="header__link header__link_is_logged-in">Сохраненные фильмы</Link>
-            <Link to="/" className="header__account-link">
+            <Link to="/movies" className="header__link header__link_is_logged-in header__link_is_active">Фильмы</Link>
+            <Link to="/saved-movies" className="header__link header__link_is_logged-in">Сохраненные фильмы</Link>
+            <Link to="/profile" className="header__account-link">
               <div className="header__account-logo"/>
               <p className="header__account-text">Аккаунт</p>
             </Link>
