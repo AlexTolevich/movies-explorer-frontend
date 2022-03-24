98b1ef42559298f4576eba08269860e3335bc388
AlexanderKuznetsov <alexanderanatolevic@yandex.ru>
2022-03-24 12:25:37 +0500

    feat: adaptiv Main on 320

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -6,10 +6,10 @@ function AboutProject() {
       <h2 className="about-project__header">О проекте</h2>
       <div className="about-project__header-line"/>
       <div className="about-project__table">
-        <h3 className="about-project__table-header">Дипломный проект включал 5 этапов</h3>
-        <h3 className="about-project__table-header">На выполнение диплома ушло 5 недель</h3>
-        <p className="about-project__table-text">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
-        <p className="about-project__table-text">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
+        <h3 className="about-project__table-header" id="head1">Дипломный проект включал 5 этапов</h3>
+        <h3 className="about-project__table-header" id="head2">На выполнение диплома ушло 5 недель</h3>
+        <p className="about-project__table-text" id="text1">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
+        <p className="about-project__table-text" id="text2">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
       </div>
       <div className="about-project__periods">
         <div className="about-project__periods-backend">1 неделя</div>
