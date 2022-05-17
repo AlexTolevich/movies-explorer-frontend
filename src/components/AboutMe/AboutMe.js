64d55aa3935529b14c90b26f50679b4c8412f9f0
Alexander <alexanderanatolevic@yandex.ru>
2022-05-17 12:36:21 +0500

    content: change the data about me

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,5 +1,5 @@
 import './AboutMe.css'
-import avatar from '../../images/ava_vitaliy.png'
+import avatar from '../../images/ava-min.jpeg'
 
 function AboutMe() {
   return (
@@ -8,21 +8,23 @@ function AboutMe() {
       <div className="about-me__header-line"/>
       <div className="about-me__container">
         <div className="about-me__text-container">
-          <h3 className="about-me__name">Виталий</h3>
-          <p className="about-me__profession">Фронтенд-разработчик, 30 лет</p>
-          <p className="about-me__about">Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть
-            жена
-            и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал в
-            компании
-            «СКБ Контур». После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с
-            постоянной работы.
+          <h3 className="about-me__name">Александр</h3>
+          <p className="about-me__profession">Начинающий Frontend разработчик, 34 года</p>
+          <p className="about-me__about">Я родился и живу в г.Красноуфимск. У меня есть жена и дочь. Я начинающий
+            frontend разработчик. Учусь писать чистый, лаконичный и понятный код. Хочу получить опыт в разработке
+            коммерческих проектов в команде с опытными разработчиками. Стремлюсь развиваться и стать отличным
+            специалистом в своей сфере.
+          </p>
+          <p className="about-me__about"> После того, как прошёл курс по веб-разработке, решил более детально
+            разобраться с React, для чего прохожу расширенный курс React-разработчик, где получу углубленные
+            теоретические и практические навыки по работе с React, TypeScript, Redux.
           </p>
           <ul className="about-me__social-list">
             <li className="about-me__social-item">
               <a className="about-me__social-link"
                  target="_blank"
                  rel="noreferrer"
-                 href="https://github.com/AlexTolevich">Facebook</a>
+                 href="https://t.me/AlexanderKuznetcov">Telegram</a>
             </li>
             <li className="about-me__social-item">
               <a className="about-me__social-link"
